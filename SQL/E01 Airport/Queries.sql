4ee030d91ca21af648fbea7b7d4fd29ff519aa23
BernardiSamuele <s.bernardi.2625@vallauri.edu>
2024-09-16 15:34:49 +0200

    SQL - E01 Airport

---
+++ SQL/E01 Airport/Queries.sql
@@ -0,0 +1,17 @@
+﻿/* Trovare le città da cui partono voli diretti a Roma, ordinate alfabeticamente */
+SELECT DepartureCity
+FROM Flights
+WHERE ArrivalCity = 'Rome'
+ORDER BY DepartureCity;
+
+/* Trovare le città con un aeroporto di cui non è noto il numero di piste */
+SELECT City
+FROM Airports
+WHERE TrackCount IS NULL;
+
+/* Di ogni volo misto (merci e passeggeri) estrarre il codice e i dati relativi al trasporto */
+SELECT Flights.*
+FROM Flights, Airplanes
+WHERE Flights.AirplaneType = Airplanes.AirplaneType
+AND Airplanes.PassengersCount IS NOT NULL
+AND Airplanes.CargoQuantity IS NOT NULL;
